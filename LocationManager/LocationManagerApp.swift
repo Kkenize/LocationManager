738106f779b52392c51ac48c17676d11cce3cf8b
YourName <Kkenizez@gmail.com>
2025-04-07 02:08:43 -0400

    Initial Commit

---
+++ LocationManager/LocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationManagerApp.swift
+//  LocationManager
+//
+//  Created by Zhejun Zhang on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
